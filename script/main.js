62240311f76d49444bb8631f5e339440aef882a5
chilijung <chilijung@gmail.com>
2014-01-02 17:31:38 +0800

    support yaw, roll, pitch plot

--- script/main.js
+++ script/main.js
@@ -1,47 +1,72 @@
 $(function() {	
 
 	var socket = io.connect('http://localhost:8000');
-	var data = []
+	var data_roll = []
+	var data_pitch = []
+	var data_yaw = []
 	socket.on('message', function (_data) {
 	    	var parse = JSON.parse(_data)
 		totalPoints = 300
 		var roll = parse.Roll
-		console.log(roll)
+		var pitch = parse.Pitch
+		var yaw = parse.Yaw
 
+		function getData(set, data) {
 
-		function getData(roll) {
-
-			if (data.length > 0)
-				data = data.slice(1);
-
-			while (data.length < totalPoints) {
-
-				var prev = data.length > 0 ? data[data.length - 1] : 50
-				data.push(roll);
+			// create the array to fit the totalPoint length
+			if(data.length < totalPoints) {
+				for (var i = totalPoints - 1; i >= 0; i--) {
+					data[i] = [i, 0]
+				};
+			} else {
+				for (var i = 1; i < totalPoints; i++) {
+					// console.log(data[i])
+					data[i - 1] = [i, data[i][1]]
+					// console.log(data[i])
+				};
+				data[totalPoints - 1] = [totalPoints, set]
 			}
 
-			var res = [];
-			for (var i = 0; i < data.length; ++i) {
-				res.push([i, data[i]])
-			}
-			console.log(res)
-			return res;
+			return data
+
+			// if (data.length > 0)
+			// 	data = data.slice(1);
+
+			// while (data.length < totalPoints) {
+			// 	var prev = data.length > 0 ? data[data.length - 1] : 50
+			// 	data.push(set);
+			// }
+
+			// var res = [];
+			// for (var i = 0; i < data.length; ++i) {
+			// 	res.push([i, data[i]])
+			// }
+			// return res;
 		}
 
-		var plot = $.plot("#signal-plot", [getData(roll)], {
+		console.log(data_roll)
+
+		var plot = $.plot("#signal-plot", [{
+				label: 'Roll',
+				data: getData(roll, data_roll)
+			},{
+				label: 'Pitch',
+				data: getData(pitch, data_pitch)
+			},{
+				label: 'Yaw',
+				data: getData(yaw, data_yaw)
+			}], {
 			series: {
 				shadowSize: 0	// Drawing is faster without shadows
 			},
 			yaxis: {
-				min: 0,
-				max: 100
+				min: -200,
+				max: 200
 			},
 			xaxis: {
 				show: false
 			}
 		});
-
-		plot.setData([getData(roll)]);
 		plot.draw();
 	});
 
