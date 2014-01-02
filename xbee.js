6f7dc1e197a1bb5bf70a855d7621678c254020f6
chilijung <chilijung@gmail.com>
2014-01-02 11:00:02 +0800

    add socket io server

--- xbee.js
+++ xbee.js
@@ -1,13 +1,44 @@
+var app = require('http').createServer(handler)
+var io = require('socket.io').listen(app)
+var fs = require('fs')
 var SerialPort = require("serialport").SerialPort
 var serialPort = new SerialPort("/dev/ttyUSB0", {
 	baudrate: 9600
 })
 
-serialPort.on("open", function() {
-	console.log("open");
-	var text = '';
-	serialPort.on("data", function(data) {
-		text = text + data
-		console.log(text)
+app.listen(80);
+
+function handler (req, res) {
+	fs.readFile(__dirname + '/index.html',
+		function(err, data) {
+			if(err) {
+				res.writeHead(500);
+				return res.end('Error loading index.html');
+			}
+			res.writeHead(200);
+			res.end(data);
+		})
+	}
+
+io.sockets.on('connection', function (socket) {
+
+	serialPort.on("open", function() {
+		console.log("open");
+		var text = '';
+		serialPort.on("data", function(data) {
+			text = text + data
+			var index = text.lastIndexOf("/r/n");
+			if(index != -1) {
+				var chop = text.substring(0, text.lastIndexOf("/r/n"))
+				var parse = JSON.parse(chop)
+				console.log(parse)
+				socket.emit('data', parse);
+				text = ''
+			}
+		});
 	});
+
+
 });
+
+	
